72d277ee20d44e01659396766467071d78ccd62d
Cody Constine <cody.constine@thetradedesk.com>
2022-07-08 11:15:21 -0600

    Added Decryption tests

--- src/test/java/com/uid2/shared/encryption/AesCbcTest.java
+++ src/test/java/com/uid2/shared/encryption/AesCbcTest.java
@@ -11,7 +11,7 @@ import java.time.Instant;
 
 public class AesCbcTest extends TestCase {
 
-    public void testEncryption() throws Exception {
+    public void testEncryptionDecryption() throws Exception {
 
         final EncryptionKey key = new EncryptionKey(1, Random.getRandomKeyBytes(), Instant.now(), Instant.now(), Instant.now(), -1);
         final String testString = "foo@bar.comasdadsjahjhafjhjkfhakjhfkjshdkjfhaskdjfh";
@@ -21,6 +21,16 @@ public class AesCbcTest extends TestCase {
 
         final String decryptedString = new String(decrypted, "UTF-8");
         Assert.assertEquals(testString, decryptedString);
+    }
+
+    public void testDecryption() {
+        byte[] keyBytes = new byte[]{-82, -66, -67, -114, 87, 24, -108, 82, -77, 112, 9, 80, 118, 39, 66, -35, 59, -81, -72, -81, 30, -41, 113, 101, -76, 79, 119, -73, 59, -39, 0, 75};
+        byte[] encypted = new byte[]{34, -42, -99, 68, 110, 49, 45, 57, 11, 64, 74, -43, 86, -73, 33, -125, -100, 2, -27, 38, 103, 97, -17, -115, -116, 10, 102, -41, -35, -53, 34, 60, -44, 59, 101, 24, -14, 9, -56, -71, 86, -31, -44, -75, -124, -77, 58, -20, 3, 26, -39, 95, 100, 24, -110, 100, 34, 25, -4, 41, -93, -3, -83, -44, 91, -1, 34, 25, 83, -58, 42, -116, 51, -112, 91, 71, 8, -25, 26, 41};
+        final EncryptionKey key = new EncryptionKey(1, keyBytes, Instant.now(), Instant.now(), Instant.now(), -1);
+        byte[] payload = AesCbc.decrypt(encypted, key);
+        String expected = "foo@bar.comasdadsjahjhafjhjkfhakjhfkjshdkjfhaskdjfh";
+        String results = new String(payload);
+        assertEquals(results, expected);
     }
 
 }
--- src/test/java/com/uid2/shared/encryption/AesGcmTest.java
+++ src/test/java/com/uid2/shared/encryption/AesGcmTest.java
@@ -7,6 +7,8 @@ import junit.framework.TestCase;
 import java.nio.charset.StandardCharsets;
 import java.time.Instant;
 
+import static org.junit.Assert.assertArrayEquals;
+
 public class AesGcmTest extends TestCase {
     public void testEncryptionDecryption() {
         final EncryptionKey key = new EncryptionKey(1, Random.getRandomKeyBytes(), Instant.now(), Instant.now(), Instant.now(), -1);
@@ -16,11 +18,21 @@ public class AesGcmTest extends TestCase {
         assertEquals(plaintxt, decryptedText);
     }
 
-    public void testEncryptionString() {
+    public void testEncryptionDecryptionString() {
         final EncryptionKey key = new EncryptionKey(1, Random.getRandomKeyBytes(), Instant.now(), Instant.now(), Instant.now(), -1);
         String plaintxt = "hello world";
         EncryptedPayload payload = AesGcm.encrypt(plaintxt, key);
         String decryptedText = new String(AesGcm.decrypt(payload.getPayload(), 0, key), StandardCharsets.UTF_8);
         assertEquals(plaintxt, decryptedText);
+    }
+
+    public void testDecryption() {
+        byte[] keyBytes = new byte[]{19, -105, -79, 100, 11, 38, -93, 100, 123, 111, 68, -57, 67, -12, -33, 14, -53, -120, -66, -116, 30, 9, -123, -50, -36, 73, 79, 85, 78, -18, 42, -108};
+        byte[] encypted = new byte[]{-2, 35, -109, -1, -123, 75, 111, 93, 83, 0, -11, -97, -69, -10, 88, 82, -62, 83, 76, -75, -51, 45, 87, 102, 38, 117, 98, 84, -78, -100, -2, -99, -3, -121, 94, 23, 75, 84, 20};
+        final EncryptionKey key = new EncryptionKey(1, keyBytes, Instant.now(), Instant.now(), Instant.now(), -1);
+        byte[] payload = AesGcm.decrypt(encypted,0, key);
+        String expected = "hello world";
+        String results = new String(payload);
+        assertEquals(results, expected);
     }
 }
