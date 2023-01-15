1d542e96a513a6c5f8e50b6b0427dfb9b1c96add
Sunny Wu <sunny.wu@thetradedesk.com>
2023-01-15 23:13:52 +1100

    revert last commit

--- src/main/java/com/uid2/shared/model/EncryptionKey.java
+++ src/main/java/com/uid2/shared/model/EncryptionKey.java
@@ -50,30 +50,4 @@ public final class EncryptionKey {
     }
 
     public boolean isExpired(Instant asOf) { return !expires.isAfter(asOf); }
-
-    @Override
-    public boolean equals(Object o) {
-        // If the object is compared with itself then return true
-        if (o == this) return true;
-
-        /* Check if o is an instance of Complex or not
-          "null instanceof [type]" also returns false */
-        if (!(o instanceof EncryptionKey)) return false;
-
-        // typecast o to Complex so that we can compare data members
-        EncryptionKey b = (EncryptionKey) o;
-
-        // Compare the data members and return accordingly
-        return this.id == b.id
-                && this.keyBytes.equals(b.keyBytes)
-                && this.created.equals(b.created)
-                && this.activates.equals(b.activates)
-                && this.expires.equals(b.expires)
-                && this.siteId == b.siteId;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, keyBytes, created, activates, expires, siteId);
-    }
 }
