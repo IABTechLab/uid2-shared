b81217dff0a93e0a39ba0727a00fc0e83239b2ee
scong-ttd <shawn.cong@thetradedesk.com>
2023-01-18 10:40:42 +0900

    enable aes-gcm as attestation token decryption and remove old methods

--- src/test/java/com/uid2/shared/secure/AttestationTokenTest.java
+++ src/test/java/com/uid2/shared/secure/AttestationTokenTest.java
@@ -1,18 +1,18 @@
 package com.uid2.shared.secure;
 
-import com.uid2.shared.attest.AttestationToken;
 import com.uid2.shared.attest.AttestationTokenService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
 
 public class AttestationTokenTest {
     private static final String ENCRYPTION_KEY = "attestation-token-secret";
     private static final String SALT = "attestation-token-salt";
     @Test
-    public void testAttestationTokenPlain() {
+    public void testAttestationToken() {
         final AttestationTokenService ats = new AttestationTokenService(ENCRYPTION_KEY, SALT);
         final String attestationToken = ats.createToken(
                 "userToken",
@@ -21,24 +21,34 @@ public class AttestationTokenTest {
     }
 
     @Test
-    public void testAttestationTokenBackwardsCompatible() {
-        // this test should deprecate together with old encode methods
+    public void testAttestationTokenBadAlgorithm() {
         final AttestationTokenService ats = new AttestationTokenService(ENCRYPTION_KEY, SALT);
-        final String attestationToken = new AttestationToken(
-                    "userToken",
-                    Instant.now().plus(1, ChronoUnit.HOURS)
-                    ).encode(ENCRYPTION_KEY, SALT);
-        Assertions.assertTrue(ats.validateToken("userToken", attestationToken));
+        final String attestationToken = ats.createToken(
+                "userToken",
+                Instant.now().plus(1, ChronoUnit.HOURS));
+        final String badAttestationToken =
+                attestationToken.substring(0, attestationToken.length() - 1) + "q";
+        Assertions.assertFalse(ats.validateToken("userToken", badAttestationToken));
     }
 
     @Test
-    public void testAttestationTokenNew() {
-        // this test should deprecate together with old encode methods
+    public void testAttestationTokenNoAlgorithm() {
         final AttestationTokenService ats = new AttestationTokenService(ENCRYPTION_KEY, SALT);
-        final String attestationToken = new AttestationToken(
+        final String attestationToken = ats.createToken(
                 "userToken",
-                Instant.now().plus(1, ChronoUnit.HOURS)
-        ).encodeNew(ENCRYPTION_KEY, SALT);
-        Assertions.assertTrue(ats.validateToken("userToken", attestationToken));
+                Instant.now().plus(1, ChronoUnit.HOURS));
+        final String badAttestationToken = String.join("-",
+                Arrays.copyOfRange(attestationToken.split("-"), 0, 2));
+        Assertions.assertFalse(ats.validateToken("userToken", badAttestationToken));
+    }
+
+    @Test
+    public void testAttestationTokenMalformed() {
+        final AttestationTokenService ats = new AttestationTokenService(ENCRYPTION_KEY, SALT);
+        final String attestationToken = ats.createToken(
+                "userToken",
+                Instant.now().plus(1, ChronoUnit.HOURS));
+        final String badAttestationToken = attestationToken + "-hoho";
+        Assertions.assertFalse(ats.validateToken("userToken", badAttestationToken));
     }
 }
