c8cdd38c9121ebb1b19ecd3801f73630a8453024
scong-ttd <shawn.cong@thetradedesk.com>
2023-01-30 09:20:25 +0800

    wrap attestation token lifetime into atService for better encapsulation

--- src/main/java/com/uid2/shared/attest/IAttestationTokenService.java
+++ src/main/java/com/uid2/shared/attest/IAttestationTokenService.java
@@ -3,6 +3,27 @@ package com.uid2.shared.attest;
 import java.time.Instant;
 
 public interface IAttestationTokenService {
+    /**
+     * Create attestation token from user token
+     * @param userToken
+     * @return attestation token
+     */
+    String createToken(String userToken);
+
+    /**
+     * Create attestation token from user token
+     * @param userToken
+     * @param expiresAt
+     * @return
+     */
+    @Deprecated
     String createToken(String userToken, Instant expiresAt);
+
+    /**
+     * Validate if attestation is generated from the user token provided
+     * @param userToken
+     * @param attestationToken
+     * @return if the credential matches
+     */
     boolean validateToken(String userToken, String attestationToken);
 }
