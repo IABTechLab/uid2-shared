d13abdac7be9469ff0ab28d7dc1a7a478aa6993c
scong-ttd <shawn.cong@thetradedesk.com>
2023-01-30 09:33:48 +0800

    test token lifetime

--- src/test/java/com/uid2/shared/secure/AttestationTokenTest.java
+++ src/test/java/com/uid2/shared/secure/AttestationTokenTest.java
@@ -1,5 +1,6 @@
 package com.uid2.shared.secure;
 
+import com.uid2.shared.attest.AttestationToken;
 import com.uid2.shared.attest.AttestationTokenService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -50,5 +51,20 @@ public class AttestationTokenTest {
                 Instant.now().plus(1, ChronoUnit.HOURS));
         final String badAttestationToken = attestationToken + "-hoho";
         Assertions.assertFalse(ats.validateToken("userToken", badAttestationToken));
+    }
+
+    @Test
+    public void testAttestationTokenNew() {
+        final long lifetime = 1800;
+        final Instant expiryLowerBound = Instant.now().plus(lifetime - 60, ChronoUnit.SECONDS);
+        final Instant expiryUpperBound = Instant.now().plus(lifetime + 300, ChronoUnit.SECONDS);
+        final AttestationTokenService ats = new AttestationTokenService(ENCRYPTION_KEY, SALT, lifetime);
+
+        final String attestationToken = ats.createToken("userToken");
+        Assertions.assertTrue(ats.validateToken("userToken", attestationToken));
+
+        final AttestationToken reconstructToken = AttestationToken.fromEncrypted(attestationToken, ENCRYPTION_KEY, SALT);
+        Assertions.assertTrue(reconstructToken.getExpiresAt().isAfter(expiryLowerBound));
+        Assertions.assertTrue(reconstructToken.getExpiresAt().isBefore(expiryUpperBound));
     }
 }
