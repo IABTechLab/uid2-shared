f3b825fb4f9945ee0dc8540b731114ebc26a1d60
Cody Constine <cody.constine@thetradedesk.com>
2023-02-06 15:52:10 -0700

    Added to interface too

--- src/main/java/com/uid2/shared/store/IKeysAclSnapshot.java
+++ src/main/java/com/uid2/shared/store/IKeysAclSnapshot.java
@@ -6,6 +6,18 @@ import com.uid2.shared.store.ACLMode.MissingAclMode;
 
 public interface IKeysAclSnapshot {
     boolean canClientAccessKey(ClientKey clientKey, EncryptionKey key);
+
+    /**
+     * This function when called with MissingAclMode.ALLOW_ALL will always return true if the key does not have an ACL
+     * When called with MissingAclMode.DENY_ALL will always return false if there is no ACL
+     * Client can always access their own keys
+     *
+     * @param clientKey the client key that is trying to access a key
+     * @param key the key the client is trying to access
+     * @param accessMethod the access mode described in the description
+     *
+     * @return Bool if client can access the given key
+     */
     boolean canClientAccessKey(ClientKey clientKey, EncryptionKey key, MissingAclMode accessMethod);
 }
 
