5cc5cd043cdc0dda03a9c4b7de582faec6fca12d
Thomas Manson <thomas.manson@thetradedesk.com>
2023-05-15 13:13:10 +1000

    Set the tags on upload, not via another api call Check versioning on upload result

--- src/main/java/com/uid2/shared/cloud/TaggableCloudStorage.java
+++ src/main/java/com/uid2/shared/cloud/TaggableCloudStorage.java
@@ -1,8 +1,14 @@
 package com.uid2.shared.cloud;
 
+import java.io.InputStream;
 import java.util.Map;
 
 public interface TaggableCloudStorage extends ICloudStorage {
 
     void setTags(String cloudPath, Map<String, String> tags) throws CloudStorageException;
+
+    void upload(String localPath, String cloudPath, Map<String, String> tags) throws CloudStorageException;
+
+    void upload(InputStream input, String cloudPath, Map<String, String> tags) throws CloudStorageException;
+
 }
