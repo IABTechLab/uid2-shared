c2e9e677e634bdda12c8223eedce76898e1b09ce
Gian Miguel Del Mundo <gianmiguel.delmundo@thetradedesk.com>
2023-06-27 11:42:20 +0800

    Updated formatting for ClientKey

--- src/main/java/com/uid2/shared/auth/ClientKey.java
+++ src/main/java/com/uid2/shared/auth/ClientKey.java
@@ -19,18 +19,26 @@ public class ClientKey implements IRoleAuthorizable<Role> {
     private final byte[] secretBytes;
     private String name;
     private String contact;
-    private final long created;
+    private final long created; // epochSeconds
     private Set<Role> roles;
     @JsonProperty("site_id")
     private int siteId;
     private boolean disabled;
 
-    public ClientKey(String key, String secret) {
+    public ClientKey(String key, String secret, String name, String contact, Instant created, Set<Role> roles, int siteId, boolean disabled) {
         this.key = key;
         this.secret = secret;
         this.secretBytes = Utils.decodeBase64String(secret);
-        created = Instant.parse("2021-01-01T00:00:00.000Z").getEpochSecond();
-        siteId = -1;
+        this.name = name;
+        this.contact = contact;
+        this.created = created.getEpochSecond();
+        this.roles = Collections.unmodifiableSet(roles);
+        this.siteId = siteId;
+        this.disabled = disabled;
+    }
+
+    public ClientKey(String key, String secret, String contact, Role... roles) {
+        this(key, secret, contact, contact, Instant.parse("2021-01-01T00:00:00.000Z"), new HashSet<>(Arrays.asList(roles)), 0, false);
     }
 
     public ClientKey(String key, String secret, Instant created) {
@@ -41,28 +49,25 @@ public class ClientKey implements IRoleAuthorizable<Role> {
         this.siteId = -1;
     }
 
-    public ClientKey withName(String name) { this.name = name; return this; }
-    public ClientKey withContact(String contact) { this.contact = contact; return this; }
-    public ClientKey withNameAndContact(String name) { this.name = this.contact = name; return this; }
-    public ClientKey withRoles(Role... roles) { this.roles = Collections.unmodifiableSet(new HashSet<>(Arrays.asList(roles))); return this; }
-    public ClientKey withRoles(Set<Role> roles) { this.roles = Collections.unmodifiableSet(roles); return this; }
-    public ClientKey withSiteId(int siteId) { this.siteId = siteId; return this; }
-
-    public ClientKey(String key, String secret, String contact, Role... roles) {
-        this(key, secret, contact, contact, Instant.parse("2021-01-01T00:00:00.000Z"), new HashSet<Role>(Arrays.asList(roles)), 0, false);
-    }
-
-    public ClientKey(String key, String secret, String name, String contact, Instant created, Set<Role> roles, int siteId,
-                     boolean disabled) {
+    public ClientKey(String key, String secret) {
         this.key = key;
         this.secret = secret;
         this.secretBytes = Utils.decodeBase64String(secret);
-        this.name = name;
-        this.contact = contact;
-        this.created = created.getEpochSecond();
-        this.roles = Collections.unmodifiableSet(roles);
-        this.siteId = siteId;
-        this.disabled = disabled;
+        created = Instant.parse("2021-01-01T00:00:00.000Z").getEpochSecond();
+        siteId = -1;
+    }
+
+    public static ClientKey valueOf(JsonObject json) {
+        return new ClientKey(
+                json.getString("key"),
+                json.getString("secret"),
+                json.getString("name"),
+                json.getString("contact"),
+                Instant.ofEpochSecond(json.getLong("created")),
+                Roles.getRoles(Role.class, json),
+                json.getInteger("site_id"),
+                json.getBoolean("disabled", false)
+        );
     }
 
     public String getKey() {
@@ -82,8 +87,23 @@ public class ClientKey implements IRoleAuthorizable<Role> {
         return name;
     }
 
+    public ClientKey withName(String name) {
+        this.name = name;
+        return this;
+    }
+
     public String getContact() {
         return contact;
+    }
+
+    public ClientKey withContact(String contact) {
+        this.contact = contact;
+        return this;
+    }
+
+    public ClientKey withNameAndContact(String name) {
+        this.name = this.contact = name;
+        return this;
     }
 
     public long getCreated() {
@@ -95,14 +115,42 @@ public class ClientKey implements IRoleAuthorizable<Role> {
     }
 
     @Override
+    public boolean hasRole(Role role) {
+        return this.roles.contains(role);
+    }
+
+    public ClientKey withRoles(Role... roles) {
+        this.roles = Collections.unmodifiableSet(new HashSet<>(Arrays.asList(roles)));
+        return this;
+    }
+
+    public ClientKey withRoles(Set<Role> roles) {
+        this.roles = Collections.unmodifiableSet(roles);
+        return this;
+    }
+
+    @Override
     public Integer getSiteId() {
         return siteId;
     }
-    public boolean hasValidSiteId() { return SiteUtil.isValidSiteId(siteId); }
-    public boolean isDisabled() { return disabled; }
-    public void setDisabled(boolean disabled) { this.disabled = disabled; }
 
-    // Overriding equals() to compare two ClientKey objects
+    public boolean hasValidSiteId() {
+        return SiteUtil.isValidSiteId(siteId);
+    }
+
+    public ClientKey withSiteId(int siteId) {
+        this.siteId = siteId;
+        return this;
+    }
+
+    public boolean isDisabled() {
+        return disabled;
+    }
+
+    public void setDisabled(boolean disabled) {
+        this.disabled = disabled;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == this) return true;
@@ -112,36 +160,18 @@ public class ClientKey implements IRoleAuthorizable<Role> {
         ClientKey b = (ClientKey) o;
 
         return this.key.equals(b.key)
-            && this.secret.equals(b.secret)
-            && this.name.equals(b.name)
-            && this.contact.equals(b.contact)
-            && this.roles.equals(b.roles)
-            && this.created == b.created
-            && this.siteId == b.siteId
-            && this.disabled == b.disabled
-            && Arrays.equals(this.secretBytes, b.secretBytes);
+                && this.secret.equals(b.secret)
+                && this.name.equals(b.name)
+                && this.contact.equals(b.contact)
+                && this.roles.equals(b.roles)
+                && this.created == b.created
+                && this.siteId == b.siteId
+                && this.disabled == b.disabled
+                && Arrays.equals(this.secretBytes, b.secretBytes);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(key, secret, name, contact, roles, created, siteId, disabled, secretBytes);
-    }
-
-    public static ClientKey valueOf(JsonObject json) {
-        return new ClientKey(
-            json.getString("key"),
-            json.getString("secret"),
-            json.getString("name"),
-            json.getString("contact"),
-            Instant.ofEpochSecond(json.getLong("created")),
-            Roles.getRoles(Role.class, json),
-            json.getInteger("site_id"),
-            json.getBoolean("disabled", false)
-        );
-    }
-
-    @Override
-    public boolean hasRole(Role role) {
-        return this.roles.contains(role);
+        return Objects.hash(key, secret, name, contact, roles, created, siteId, disabled, Arrays.hashCode(secretBytes));
     }
 }
