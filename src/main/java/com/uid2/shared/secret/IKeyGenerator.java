75746ffd7d8639a88ca693eb0719deb6f6c5b6b5
Thomas Manson <thomas.manson@thetradedesk.com>
2023-07-07 11:58:11 +1000

    Added comment

--- src/main/java/com/uid2/shared/secret/IKeyGenerator.java
+++ src/main/java/com/uid2/shared/secret/IKeyGenerator.java
@@ -3,5 +3,9 @@ package com.uid2.shared.secret;
 public interface IKeyGenerator {
     byte[] generateRandomKey(int keyLen) throws Exception;
     String generateRandomKeyString(int keyLen) throws Exception;
+
+    // To make secrets easier to scan for using a RegEx and reduce the likelihood of keys being leaked, this
+    // inserts a . as the 6th character. If the generated string is shorter than 6 characters, no change is made,
+    // and this would return the same as generateRandomKey
     String generateFormattedKeyString(int keyLen) throws Exception;
 }
