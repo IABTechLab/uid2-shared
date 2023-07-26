5368ef153c3d5da93a6e9333bf70ff4573551425
Katherine Chen <katherine.chen@thetradedesk.com>
2023-07-26 10:00:09 +1000

    Add `AttestationTokenRetrieverException` exception

---
+++ src/main/java/com/uid2/shared/attest/AttestationTokenRetrieverException.java
@@ -0,0 +1,22 @@
+package com.uid2.shared.attest;
+
+public class AttestationTokenRetrieverException extends Exception {
+    private int statusCode = 0;
+
+    public AttestationTokenRetrieverException(Throwable t) {
+        super(t);
+    }
+
+    public AttestationTokenRetrieverException(String message) {
+        super(message);
+    }
+
+    public AttestationTokenRetrieverException(int statusCode, String message) {
+        super("http status: " + String.valueOf(statusCode) + ", " + message);
+        this.statusCode = statusCode;
+    }
+
+    public boolean couldBeAttestationFailure() {
+        return statusCode == 401;
+    }
+}
