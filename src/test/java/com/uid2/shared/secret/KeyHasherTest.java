a6251ddd83154a8e46b3da7d9dc45a61c0f16fde
Vishal Egbert <vishal.egbert@thetradedesk.com>
2023-08-15 15:39:19 +1000

    Clean up

--- src/test/java/com/uid2/shared/secret/KeyHasherTest.java
+++ src/test/java/com/uid2/shared/secret/KeyHasherTest.java
@@ -2,9 +2,7 @@ package com.uid2.shared.secret;
 
 import org.junit.Test;
 
-import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
-import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 
 import static org.junit.Assert.assertEquals;
@@ -12,14 +10,14 @@ import static org.junit.Assert.assertNotEquals;
 
 public class KeyHasherTest {
     @Test
-    public void HashKey_ReturnsKnownHash_WithGivenSalt() throws NoSuchAlgorithmException, UnsupportedEncodingException {
+    public void HashKey_ReturnsKnownHash_WithGivenSalt() {
         KeyHasher hasher = new KeyHasher();
         byte[] hashedBytes = hasher.hashKey("test-key", "test-salt".getBytes(StandardCharsets.UTF_8));
         assertEquals("hzXFALLdI9ji4ajnzhWdbEQNci+kAoA40Ie6X7bEyjIvMFbhQfYZC1sTPeK+14QM+Ox2a6wJ0U2fLzqnoUgCbQ==", Base64.getEncoder().encodeToString(hashedBytes));
     }
 
     @Test
-    public void HashKey_ReturnsNewHashEverytime_WithRandomSalt() throws NoSuchAlgorithmException, UnsupportedEncodingException {
+    public void HashKey_ReturnsNewHashEverytime_WithRandomSalt() {
         KeyHasher hasher = new KeyHasher();
         KeyHashResult result1 = hasher.hashKey("test--key");
         KeyHashResult result2 = hasher.hashKey("test--key");
