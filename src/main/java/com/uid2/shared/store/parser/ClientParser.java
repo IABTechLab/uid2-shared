a6ee624805cd8b969ff020ab423dfb13f34f8624
Vishal Egbert <vishal.egbert@thetradedesk.com>
2023-09-13 17:11:15 +1000

    Parse to collection of client keys

--- src/main/java/com/uid2/shared/store/parser/ClientParser.java
+++ src/main/java/com/uid2/shared/store/parser/ClientParser.java
@@ -7,13 +7,14 @@ import com.uid2.shared.utils.ObjectMapperFactory;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
-public class ClientParser implements Parser<List<ClientKey>> {
+public class ClientParser implements Parser<Collection<ClientKey>> {
     private static final ObjectMapper OBJECT_MAPPER = ObjectMapperFactory.build();
 
     @Override
-    public ParsingResult<List<ClientKey>> deserialize(InputStream inputStream) throws IOException {
+    public ParsingResult<Collection<ClientKey>> deserialize(InputStream inputStream) throws IOException {
         ClientKey[] clientKeys = OBJECT_MAPPER.readValue(inputStream, ClientKey[].class);
         return new ParsingResult<>(Arrays.asList(clientKeys), clientKeys.length);
     }
