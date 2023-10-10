a421f0d4e65353dbaaaff3bb47ebc5046f054c8b
Lun Wang <lun.wang@thetradedesk.com>
2023-10-10 09:05:56 +0800

    Azure CC attestation provider

--- src/main/java/com/uid2/shared/attest/AttestationFactory.java
+++ src/main/java/com/uid2/shared/attest/AttestationFactory.java
@@ -29,4 +29,14 @@ public class AttestationFactory {
         Constructor<?> c = cls.getConstructor();
         return (IAttestationProvider) c.newInstance();
     }
+
+    public static IAttestationProvider getAzureCCAttestation() throws Exception {
+        return getAzureCCAttestation(null);
+    }
+
+    public static IAttestationProvider getAzureCCAttestation(String maaEndpoint) throws Exception {
+        Class<?> cls = Class.forName("com.uid2.attestation.azure.AzureCCAttestationProvider");
+        Constructor<?> c = cls.getConstructor(String.class);
+        return (IAttestationProvider) c.newInstance(maaEndpoint);
+    }
 }
