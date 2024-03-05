5dea4369e63e8515b30b7a1a6d7e662e8c3c090e
Asloob Qureshi <asloobq@gmail.com>
2024-03-05 11:46:26 -0800

    [UID2-2892] Add new roles for okta groups (#203)
    * Add new roles for okta groups
    
    * Rename ALL to Default
    
    * rename Default to Maintainer

--- src/main/java/com/uid2/shared/auth/Role.java
+++ src/main/java/com/uid2/shared/auth/Role.java
@@ -10,13 +10,21 @@ public enum Role {
     OPERATOR,
     OPTOUT, // Used for operators to call optout service's /optout/refresh API
     OPTOUT_SERVICE, // UID2-912 - Specifically for optout service only
+    @Deprecated
     CLIENTKEY_ISSUER,
+    @Deprecated
     OPERATOR_MANAGER,
+    @Deprecated
     SECRET_MANAGER,
+    @Deprecated
     ADMINISTRATOR,
-    SHARING_PORTAL,
+    SHARING_PORTAL, // corresponds to custom okta scope 'uid2.admin.ss-portal'
     PRIVATE_SITE_REFRESHER, // UID2-575 - For cronjob to trigger private site data generated
-
+    MAINTAINER, // corresponds to 'developer' okta group
+    PRIVILEGED, // corresponds to 'developer-elevated' okta group
+    SUPER_USER, // corresponds to 'admin' okta group
+    SECRET_ROTATION, // corresponds to custom okta scope 'uid2.admin.secret-rotation'
+    PRIVATE_OPERATOR_SYNC, // corresponds to custom okta scope 'uid2.admin.site-sync'
     @JsonEnumDefaultValue
     UNKNOWN
 }
