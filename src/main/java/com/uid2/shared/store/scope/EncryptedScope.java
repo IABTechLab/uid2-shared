4af6e8dac18ea77e71682bb53994ad9ad5f76a59
lizk886 <wayzheng@usc.edu>
2024-07-01 17:38:50 -0600

    changes implementation

--- src/main/java/com/uid2/shared/store/scope/EncryptedScope.java
+++ src/main/java/com/uid2/shared/store/scope/EncryptedScope.java
@@ -1,21 +1,30 @@
+
 package com.uid2.shared.store.scope;
+
 
 import com.uid2.shared.store.CloudPath;
 
-public class EncryptedScope extends SiteScope {
-
-    public EncryptedScope(CloudPath rootMetadataPath, Integer siteId) {
-        super(rootMetadataPath, siteId);
+public class EncryptedScope implements StoreScope {
+    public EncryptedScope(CloudPath rootMetadataPath, Integer siteId){
+        this.rootMetadataPath = rootMetadataPath;
+        this.siteId = siteId;
     }
+    private final Integer siteId;
+    private final CloudPath rootMetadataPath;
 
     @Override
     public CloudPath getMetadataPath() {
-        return resolve(super.getMetadataPath().getFileName());
+        return resolve(rootMetadataPath.getFileName());
     }
 
     @Override
     public CloudPath resolve(CloudPath cloudPath) {
-        CloudPath directory = super.getMetadataPath().getParent().getParent();
+        CloudPath directory = rootMetadataPath.getParent();
         return directory.resolve("encryption").resolve("site").resolve(getId().toString()).resolve(cloudPath);
+    }
+
+    @Override
+    public Integer getId() {
+        return siteId;
     }
 }
