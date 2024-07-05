df6d07ee4be3bbc6d6336545161fbfababd396c7
lizk886 <wayzheng@usc.edu>
2024-07-05 13:36:54 -0600

    let scope takes in public or private

--- src/test/java/com/uid2/shared/store/scope/EncryptedScopeTest.java
+++ src/test/java/com/uid2/shared/store/scope/EncryptedScopeTest.java
@@ -24,17 +24,17 @@ class EncryptedScopeTest {
 
     @Test
     void getMetadataPathPublic() {
-        EncryptedScope scope = new EncryptedScope(new CloudPath("/original/path/metadata.json"), 5);
+        EncryptedScope scope = new EncryptedScope(new CloudPath("/original/path/metadata.json"), 5, true);
         CloudPath expected = new CloudPath("/original/path/encrypted/5_public/metadata.json");
 
-        CloudPath actual = scope.getMetadataPath(true);
+        CloudPath actual = scope.getMetadataPath();
         assertThat(actual).isEqualTo(expected);
     }
 
     @Test
     void resolvesPathsRelativeToSiteDirectoryPublic() {
-        EncryptedScope scope = new EncryptedScope(new CloudPath("/original/path/metadata.json"), 5);
-        CloudPath actual = scope.resolve(new CloudPath("file.xyz"), true);
+        EncryptedScope scope = new EncryptedScope(new CloudPath("/original/path/metadata.json"), 5, true);
+        CloudPath actual = scope.resolve(new CloudPath("file.xyz"));
         CloudPath expected = new CloudPath("/original/path/encrypted/5_public/file.xyz");
         assertThat(actual).isEqualTo(expected);
     }
