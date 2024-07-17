58ddfc4f5e1706e820f64178bd0777cc243f9536
lizk886 <wayzheng@usc.edu>
2024-07-16 22:39:48 -0600

    let s3 key parser deserealize more items

--- src/test/java/com/uid2/shared/store/parser/S3KeyParserTest.java
+++ src/test/java/com/uid2/shared/store/parser/S3KeyParserTest.java
@@ -94,4 +94,62 @@ class S3KeyParserTest {
         String expectedJson = "{\"id\":1,\"siteId\":999,\"activates\":1718689091,\"created\":1718689091,\"secret\":\"64bNHMpU/mjaywjOpVacFOvEIFZmbYYUsNVNVu1jJZs=\"}";
         assertEquals(expectedJson, jsonString);
     }
+
+    @Test
+    void testDeserializeEndpointResults() throws IOException {
+        String json = "[\n" +
+                "        {\n" +
+                "            \"id\": 1,\n" +
+                "            \"siteId\": 999,\n" +
+                "            \"activates\": 1720641670,\n" +
+                "            \"created\": 1720641670,\n" +
+                "            \"secret\": \"mydrCudb2PZOm01Qn0SpthltmexHUAA11Hy1m+uxjVw=\"\n" +
+                "        },\n" +
+                "        {\n" +
+                "            \"id\": 2,\n" +
+                "            \"siteId\": 999,\n" +
+                "            \"activates\": 1720728070,\n" +
+                "            \"created\": 1720641670,\n" +
+                "            \"secret\": \"FtdslrFSsvVXOuhOWGwEI+0QTkCvM8SGZAP3k2u3PgY=\"\n" +
+                "        },\n" +
+                "        {\n" +
+                "            \"id\": 3,\n" +
+                "            \"siteId\": 999,\n" +
+                "            \"activates\": 1720814470,\n" +
+                "            \"created\": 1720641670,\n" +
+                "            \"secret\": \"/7zO6QbKrhZKIV36G+cU9UR4hZUVg5bD+KjbczICjHw=\"\n" +
+                "        }\n" +
+                "    ]";
+        InputStream inputStream = new ByteArrayInputStream(json.getBytes(StandardCharsets.UTF_8));
+
+        ParsingResult<Map<Integer, S3Key>> result = parser.deserialize(inputStream);
+
+        assertNotNull(result);
+        assertEquals(3, result.getCount());
+        assertEquals(3, result.getData().size());
+        assertTrue(result.getData().containsKey(1));
+        assertTrue(result.getData().containsKey(2));
+        assertTrue(result.getData().containsKey(3));
+
+        S3Key key1 = result.getData().get(1);
+        assertEquals(1, key1.getId());
+        assertEquals(999, key1.getSiteId());
+        assertEquals(1720641670L, key1.getActivates());
+        assertEquals(1720641670L, key1.getCreated());
+        assertEquals("mydrCudb2PZOm01Qn0SpthltmexHUAA11Hy1m+uxjVw=", key1.getSecret());
+
+        S3Key key2 = result.getData().get(2);
+        assertEquals(2, key2.getId());
+        assertEquals(999, key2.getSiteId());
+        assertEquals(1720728070L, key2.getActivates());
+        assertEquals(1720641670L, key2.getCreated());
+        assertEquals("FtdslrFSsvVXOuhOWGwEI+0QTkCvM8SGZAP3k2u3PgY=", key2.getSecret());
+
+        S3Key key3 = result.getData().get(3);
+        assertEquals(3, key3.getId());
+        assertEquals(999, key3.getSiteId());
+        assertEquals(1720814470L, key3.getActivates());
+        assertEquals(1720641670L, key3.getCreated());
+        assertEquals("/7zO6QbKrhZKIV36G+cU9UR4hZUVg5bD+KjbczICjHw=", key3.getSecret());
+    }
 }
