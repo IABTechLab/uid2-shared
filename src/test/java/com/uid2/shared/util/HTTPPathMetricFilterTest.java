78599eb545000381de2770956a01b40c6afb3206
sophia chen <sophia.chen@thetradedesk.com>
2025-01-03 17:29:10 +1100

    Enabled path normalisation filtering for paths with params

--- src/test/java/com/uid2/shared/util/HTTPPathMetricFilterTest.java
+++ src/test/java/com/uid2/shared/util/HTTPPathMetricFilterTest.java
@@ -9,7 +9,7 @@ import java.util.Set;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HTTPPathMetricFilterTest {
-    final Set<String> pathSet = Set.of("/v1/identity/map", "/token/refresh");
+    final Set<String> pathSet = Set.of("/v1/identity/map", "/token/refresh", "/list", "/list/:siteId/:keyId");
 
     @ParameterizedTest
     @ValueSource(strings = {
@@ -18,6 +18,7 @@ public class HTTPPathMetricFilterTest {
             "/unknown-path",
             "../",
             "/v1/identity/map%55",
+            "/list/123",
     })
     void testPathFiltering_InvalidPaths_Unknown(String actualPath) {
         String filteredPath = HTTPPathMetricFilter.filterPath(actualPath, pathSet);
@@ -28,6 +29,7 @@ public class HTTPPathMetricFilterTest {
     @ValueSource(strings = {
             "v1/identity/map?id=bad-escape-code%2",
             "token/refresh?refresh_token=SOME_TOKEN<%=7485*4353%>",
+            "list/12%4/5435"
     })
     void testPathFiltering_InvalidPaths_ParsingError(String actualPath) {
         String filteredPath = HTTPPathMetricFilter.filterPath(actualPath, pathSet);
@@ -44,6 +46,8 @@ public class HTTPPathMetricFilterTest {
             "/v1/identity/new/path/../../map, /v1/identity/map",
             "token/refresh?refresh_token=123%20%23, /token/refresh",
             "v1/identity/map?identity/../map/, /v1/identity/map",
+            "/list, /list",
+            "/list/123/key123, /list/:siteId/:keyId"
 
     })
     void testPathFiltering_ValidPaths_KnownEndpoints(String actualPath, String expectedFilteredPath) {
