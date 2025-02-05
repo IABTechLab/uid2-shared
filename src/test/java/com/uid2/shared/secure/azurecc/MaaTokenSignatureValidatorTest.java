b0680842f43229947065bec5fe6d960fb7cf65e2
Katherine Chen <katherine.chen@thetradedesk.com>
2025-02-06 08:07:39 +1100

    Use ParameterizedTest to test payload

--- src/test/java/com/uid2/shared/secure/azurecc/MaaTokenSignatureValidatorTest.java
+++ src/test/java/com/uid2/shared/secure/azurecc/MaaTokenSignatureValidatorTest.java
@@ -3,17 +3,18 @@ package com.uid2.shared.secure.azurecc;
 import com.uid2.shared.secure.AttestationException;
 import com.uid2.shared.secure.TestClock;
 import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static com.uid2.shared.secure.TestUtils.loadFromJson;
 import static com.uid2.shared.secure.azurecc.MaaTokenUtils.validateAndParseToken;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MaaTokenSignatureValidatorTest {
-    @Test
-    public void testAciPayload() throws Exception {
+    @ParameterizedTest
+    @ValueSource(strings = {"/com.uid2.shared/test/secure/azurecc/jwt_payload_aci.json", "/com.uid2.shared/test/secure/azurecc/jwt_payload_aks.json"})
+    public void testPayload(String payloadPath) throws Exception {
         // expire at 1695313895
-        var payloadPath = "/com.uid2.shared/test/secure/azurecc/jwt_payload_aci.json";
         var payload = loadFromJson(payloadPath);
         var clock = new TestClock();
         clock.setCurrentTimeMs(1695313893000L);
@@ -23,27 +24,6 @@ public class MaaTokenSignatureValidatorTest {
         var expectedPublicKey = "abc";
 
         var tokenPayload = validateAndParseToken(payload, clock, "azure-cc");
-        assertEquals(true, tokenPayload.isSevSnpVM());
-        assertEquals(true, tokenPayload.isUtilityVMCompliant());
-        assertEquals(false, tokenPayload.isVmDebuggable());
-        assertEquals(expectedCcePolicy, tokenPayload.getCcePolicyDigest());
-        assertEquals(expectedLocation, tokenPayload.getRuntimeData().getLocation());
-        assertEquals(expectedPublicKey, tokenPayload.getRuntimeData().getPublicKey());
-    }
-
-    @Test
-    public void testAksPayload() throws Exception {
-        // expire at 1695313895
-        var payloadPath = "/com.uid2.shared/test/secure/azurecc/jwt_payload_aks.json";
-        var payload = loadFromJson(payloadPath);
-        var clock = new TestClock();
-        clock.setCurrentTimeMs(1695313893000L);
-
-        var expectedCcePolicy = "fef932e0103f6132437e8a1223f32efc4bea63342f893b5124645224ef29ba73";
-        var expectedLocation = "East US";
-        var expectedPublicKey = "abc";
-
-        var tokenPayload = validateAndParseToken(payload, clock, "azure-cc-aks");
         assertEquals(true, tokenPayload.isSevSnpVM());
         assertEquals(true, tokenPayload.isUtilityVMCompliant());
         assertEquals(false, tokenPayload.isVmDebuggable());
