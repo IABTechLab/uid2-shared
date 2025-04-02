2685c228898e3e64c07f410f0694cb23006e7f7f
Gian Miguel Del Mundo <gianmiguel.delmundo@thetradedesk.com>
2025-04-02 13:40:40 +0800

    Added simulated implementation of refresh_from

--- src/main/java/com/uid2/shared/model/SaltEntry.java
+++ src/main/java/com/uid2/shared/model/SaltEntry.java
@@ -1,31 +1,25 @@
 package com.uid2.shared.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
 public class SaltEntry {
     private final long id;
     private final String hashedId;
     private final long lastUpdated;
     private final String salt;
+    @Setter private long refreshFrom;
+
+    public SaltEntry(long id, String hashedId, long lastUpdated, String salt, long refreshFrom) {
+        this.id = id;
+        this.hashedId = hashedId;
+        this.lastUpdated = lastUpdated;
+        this.salt = salt;
+        this.refreshFrom = refreshFrom;
+    }
 
     public SaltEntry(long id, String hashedId, long lastUpdated, String salt) {
-        this.id = id;
-        this.lastUpdated = lastUpdated;
-        this.hashedId = hashedId;
-        this.salt = salt;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public String getHashedId() {
-        return hashedId;
-    }
-
-    public long getLastUpdated() {
-        return lastUpdated;
-    }
-
-    public String getSalt() {
-        return salt;
+        this(id, hashedId, lastUpdated, salt, 0);
     }
 }
