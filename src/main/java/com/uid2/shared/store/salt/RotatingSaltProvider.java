a38a441ee3c3c37f11ec4a9416bdebccce774c1c
way zheng <way.zheng@thetradedesk.com>
2025-09-22 12:30:27 -0700

    formatting

--- src/main/java/com/uid2/shared/store/salt/RotatingSaltProvider.java
+++ src/main/java/com/uid2/shared/store/salt/RotatingSaltProvider.java
@@ -129,12 +129,11 @@ public class RotatingSaltProvider implements ISaltProvider, IMetadataVersionedSt
 
         final String path = spec.getString("location");
         Integer size = spec.getInteger("size");
-        
         final long downloadStart = System.currentTimeMillis();
         SaltEntry[] entries = readInputStream(this.contentStreamProvider.download(path), saltFileParser, size);
         final long downloadEnd = System.currentTimeMillis();
-        
-        LOGGER.info("Salt file downloaded in {} ms", downloadEnd - downloadStart);
+        LOGGER.info("Loaded {} salts", size);
+        LOGGER.info("Salt file downloaded: {} in {} ms", contentStreamProvider.mask(path), downloadEnd - downloadStart);
         return new SaltSnapshot(effective, expires, entries, firstLevelSalt);
     }
 
