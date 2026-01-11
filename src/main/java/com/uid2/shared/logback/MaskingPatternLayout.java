ad9728ffe481ebe586a1947bea3944046782ad7b
Samin Rahman <samin.rahman@thetradedesk.com>
2026-01-12 09:40:02 +1100

    Renamed variables and added comments

--- src/main/java/com/uid2/shared/logback/MaskingPatternLayout.java
+++ src/main/java/com/uid2/shared/logback/MaskingPatternLayout.java
@@ -3,13 +3,12 @@ package com.uid2.shared.logback;
 import ch.qos.logback.classic.PatternLayout;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 
-import java.util.Map;
 import java.util.regex.Pattern;
 
 public class MaskingPatternLayout extends PatternLayout {
-    private static final Pattern maskPattern = Pattern.compile("\\S+s3\\.amazonaws\\.com/\\S*X-Amz-Security-Token=\\S+");
-    private static final String maskBroadCheckSubstring = "X-Amz-Security-Token=";
-    private static final String maskedRedaction = "REDACTED - S3";
+    private static final Pattern AWS_PRESIGNED_URL_REGEX_PATTERN = Pattern.compile("\\S+s3\\.amazonaws\\.com/\\S*X-Amz-Security-Token=\\S+");
+    private static final String AWS_SECURITY_TOKEN_HEADER = "X-Amz-Security-Token=";
+    private static final String AWS_PRESIGNED_URL_REDACTION_MASK = "REDACTED - S3";
 
     @Override
     public String doLayout(ILoggingEvent event) {
@@ -22,8 +21,9 @@ public class MaskingPatternLayout extends PatternLayout {
         }
 
         String maskedMessage = message;
-        if (maskedMessage.contains(maskBroadCheckSubstring)) {
-            maskedMessage = maskPattern.matcher(maskedMessage).replaceAll(maskedRedaction);
+        // Perform a broad check to potentially skip regex
+        if (maskedMessage.contains(AWS_SECURITY_TOKEN_HEADER)) {
+            maskedMessage = AWS_PRESIGNED_URL_REGEX_PATTERN.matcher(maskedMessage).replaceAll(AWS_PRESIGNED_URL_REDACTION_MASK);
         }
 
         return maskedMessage;
